855acd601b2f7c9f667d95d5dea935a2903dbd45
Nabila <sharmin_cse@istt.edu.bd>
2026-01-25 23:07:53 +0600

    Add files via upload

---
+++ blood donate.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int T;
+    scanf("%d",&T);
+    for(int t=0;t<T;t++){
+            int N,X;
+    scanf("%d %d",&N,&X);
+    int ter;
+    ter = N-X;
+    printf("%d\n",ter);
+
+    }
+return 0;
+}
