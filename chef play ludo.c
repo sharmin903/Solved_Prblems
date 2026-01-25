0004fc93faa00c8fab30eb637a899fc7becaacf8
Nabila <sharmin_cse@istt.edu.bd>
2026-01-25 23:08:27 +0600

    Add files via upload

---
+++ chef play ludo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+    int T;
+    scanf("%d",&T);
+    for(int t=0;t<T;t++){
+    int X;
+    scanf("%d",&X);
+        if(X==6){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
